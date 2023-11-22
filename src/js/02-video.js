ca535a09db30a7c0d8630f12380e4c23887061f9
Sad0328 <tours@soledad.com.ua>
2023-11-22 15:28:42 +0200

    h

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.getElementById('vimeo-player');
-const player = new Player(iframe);
+const vimeoPlayer = document.getElementById('vimeo-player');
+const player = new Player(vimeoPlayer);
 
 document.addEventListener('DOMContentLoaded', () => {
   const savedTime = localStorage.getItem('videoplayer-current-time');
@@ -11,11 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-const saveTimeThrottled = throttle(currentTime => {
+const saveTime = throttle(currentTime => {
   localStorage.setItem('videoplayer-current-time', currentTime);
 }, 1000);
 
 player.on('timeupdate', data => {
   const currentTime = data.seconds;
-  saveTimeThrottled(currentTime);
+  saveTime(currentTime);
 });
