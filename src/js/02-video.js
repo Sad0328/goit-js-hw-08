dd90ec0247e89a167ee082cfc74d32e4a64474dc
Sad0328 <tours@soledad.com.ua>
2023-11-22 15:01:18 +0200

    d

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const saveTimeThrottled = throttle(currentTime => {
   localStorage.setItem('videoplayer-current-time', currentTime);
-}, 1000);
+}, 2000);
 
 player.on('timeupdate', data => {
   const currentTime = data.seconds;
